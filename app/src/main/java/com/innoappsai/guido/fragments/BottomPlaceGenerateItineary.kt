e42eab768e1255434344997050a8f1f47cd3e9b2
joydip <joydipbose999@gmail.com>
2023-10-12 17:11:24 +0530

    Create Layout For Place Itineary Generation

--- app/src/main/java/com/innoappsai/guido/fragments/BottomPlaceGenerateItineary.kt
+++ app/src/main/java/com/innoappsai/guido/fragments/BottomPlaceGenerateItineary.kt
@@ -1,24 +1,18 @@
 package com.innoappsai.guido.fragments
 
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.fragment.app.activityViewModels
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.innoappsai.guido.adapters.PlacesGroupListAdapter
-import com.innoappsai.guido.collectIn
-import com.innoappsai.guido.databinding.BottomsheetPalceOptionsBinding
 import com.innoappsai.guido.databinding.BottomsheetPalceSortOptionsBinding
-import com.innoappsai.guido.model.placesUiModel.PlaceUiModel
-import com.innoappsai.guido.showToast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class BottomPlaceSortOptions : BottomSheetDialogFragment() {
+class BottomPlaceGenerateItineary : BottomSheetDialogFragment() {
 
 
 
